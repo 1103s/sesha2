7689e1002937e396156fc9b0ec053660978c65e5
Natalie Schneider <natalie.schneider@student.nmt.edu>
2021-10-13 19:20:43 -0600

    adding javascript to generate the main section

--- Prototype/resources/global-js.js
+++ Prototype/resources/global-js.js
@@ -7,12 +7,37 @@ var categories = [
 	"Programming"	
 ];
 var courses = [
-	[1,"The Futures Channel"]
-	[2,"Emusikids"]
+	[0,"The Futures Channel","This is a description of what this course is about"],
+	[1,"Emusikids","This is a description of what this course is about"],
+	[2,"The Futures Channel","This is a description of what this course is about"],
+	[3,"Emusikids","This is a description of what this course is about"],
+	[4,"The Futures Channel","This is a description of what this course is about"],
+	[5,"Emusikids","This is a description of what this course is about"],
+	[6,"The Futures Channel","This is a description of what this course is about"],
+	[7,"Emusikids","This is a description of what this course is about"],
+	[8,"The Futures Channel","This is a description of what this course is about"],
+	[9,"Emusikids","This is a description of what this course is about"],
+	[10,"The Futures Channel","This is a description of what this course is about"],
+	[11,"Emusikids","This is a description of what this course is about"],
+	[12,"The Futures Channel","This is a description of what this course is about"],
+	[13,"Emusikids","This is a description of what this course is about"]
 ];
 var courseCategories = [
-	[1,"K-8 STEM"],
-	[2,"Music"]
+	[0,"K-8 STEM"],
+	[1,"Music"],
+	[2,"K-8 STEM"],
+	[3,"Music"],
+	[4,"K-8 STEM"],
+	[4,"Programming"],
+	[5,"Music"],
+	[6,"K-8 STEM"],
+	[7,"Music"],
+	[8,"K-8 STEM"],
+	[9,"Music"],
+	[10,"K-8 STEM"],
+	[11,"Music"],
+	[12,"K-8 STEM"],
+	[13,"Music"]
 ];
 
 
@@ -21,7 +46,34 @@ const typeHandler = function(e) {
 }
 
 function displayHome(){
+	var mainHtml = document.getElementById("main");
 	
+	mainHtml.appendChild(createSection("K-8 STEM",4));
+	mainHtml.appendChild(createSection("Music",4));
+	
+	document.getElementById("reset").style.visibility='hidden';
+	document.getElementById("resetTag").style.visibility='hidden';
+	document.getElementById("about").innerHTML = "";
+	
+	
+}
+
+function createSection(category,length){
+	var categorySection = document.createElement('section');
+	categorySection.className = "category";
+	var tempString = "<h2 class=\"categoryTitle\">" + category + "</h2>";
+	
+	for(var i in courseCategories)
+	{
+		if(courseCategories[i][1] == category && length>0){
+			length--;
+			tempString += "<img class=\"thumb\" src=\"./resources/images/" + courseCategories[i][0] +" - thumbnail.png\">";
+			tempString += "<div class=\"textInfo\"> <div class=\"title\">" +  courses[courseCategories[i][0]][1] + "</div>";
+			tempString += "<div class=\"description\">" +  courses[courseCategories[i][0]][2] + "</div></div>";			
+		}
+	}
+	categorySection.innerHTML = tempString;
+	return categorySection;
 }
 
 function expandSection(section){
